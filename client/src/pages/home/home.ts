118107e76f1c9c55f668af8c7756c78004ad80e0
Valeria <valeriasalasp1997@gmail.com>
2018-10-28 22:14:37 -0400

    Update home and create note page
    creation of home menu and page to add note

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ViewController } from 'ionic-angular';
+import { NavController, ViewController, AlertController } from 'ionic-angular';
 import {CreatePage} from '../create/create';
 @Component({
   selector: 'page-home',
@@ -7,7 +7,7 @@ import {CreatePage} from '../create/create';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, private viewCtrl: ViewController, public alertCtrl: AlertController) {
 
   }
   goCreate(){
@@ -16,5 +16,25 @@ export class HomePage {
   ionViewWillEnter() {
     this.viewCtrl.showBackButton(false);
   }
-
+  showConfirm() {
+    const confirm = this.alertCtrl.create({
+      title: 'Are you sure you want to log out?',
+      /*message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',*/
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
